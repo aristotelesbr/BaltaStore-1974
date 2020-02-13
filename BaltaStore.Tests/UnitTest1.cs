ef11b8317c2ad8dbf27086e82c81e81213d025c8
Aristoteles <aristotelesbr2014@gmail.com>
2020-02-12 23:14:10 -0300

    Added attributes requireds to initialize customer

--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -1,13 +1,22 @@
+using BaltaStore.Domain.StoreContext.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BaltaStore.Tests
 {
-    [TestClass]
-    public class UnitTest1
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+      var customer = new Customer(
+          "aristoteles",
+          "Coutinho",
+          "123",
+          "mail@mail.com",
+          "86999999999",
+          "Rua dos devs 8471"
+        );
     }
+  }
 }
